3cf32ad64fcafabe69a95a73bc87162d42d4e8bf
LasseKau <44494614+LasseKau@users.noreply.github.com>
2021-04-10 05:55:38 +0300

    axios
    -

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css';
 
 function App() {
+
+  const [carName, setCarName] = useState('')
+  const [review, setReview] = useState('')
+
   return (
     <div className="App">
       <h1>CRUD APP</h1>
 
       <div className="form">
-      <label>Movie Name: </label>
-      <input type="text" name="movieName" />
+      <label>Car Name: </label>
+      <input type="text" name="carName" onChange={(e)=> {
+      setCarName(e.target.value)
+      }}/>
       <label>Review: </label>
-      <input type="text" name="review"/>
-
+      <input type="text" name="review" onChange={(e)=> {
+      setReview(e.target.value)}}/>
       <button>Submit</button>
       </div>
     </div>
