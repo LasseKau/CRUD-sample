85d8ea3728f5818ba4c4164aa43d144abc5f67c7
LasseKau <44494614+LasseKau@users.noreply.github.com>
2021-04-10 14:40:42 +0300

    update
    -

--- client/src/App.js
+++ client/src/App.js
@@ -4,75 +4,237 @@ import Axios from 'axios';
 
 function App() {
 
-  const [carName, setCarName] = useState('')
-  const [review, setReview] = useState('')
-  const [carReviewList, setReviewList] = useState([])
-  const [newReview, setNewReview] = useState([]);
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState(0);
+  const [maker, setMaker] = useState("");
+  const [review, setReview] = useState("");
+  const [year, setYear] = useState(0);
 
- // requesting back from backend, responce variable = data passed through
-  useEffect(()=> {
-    Axios.get('https://http//localhost:3001/api/get').then((response)=> {
-      console.log(response.data);
-      Set.CarReviewList(response.data);
-    });
-  }, []);
+  const [newPrice, setNewPrice] = useState(0);//state created for changed input
+
+  const [carList, setCarList] = useState([]); 
+
+//  // requesting back from backend, responce variable = data passed through
+//   useEffect(()=> {
+//     Axios.get('https://http//localhost:3001/api/get').then((response)=> {
+//       console.log(response.data);
+//       Set.CarReviewList(response.data);
+//     });
+//   }, []);
+
 
   //when the backend is requesting the name of the body from the backend, 
   //were requesting a variable that is going to be passed by the axios.post
-  const submitReview = ()=> {
-    Axios.post("http://localhost:3001/api/insert", {
-      carName: carName,
+
+  const addCar = () => {
+    Axios.post("http://localhost:3001/create", {
+      
+      carName: name,
+      carYear: year,
+      carMaker: maker,
       carReview: review,
-    });
-
-    setReviewList([...carReviewList, 
-      {movieName: carName, carReview: review},
+      carPrice: price,
+    }).then(() => {
+      setCarList([
+        ...carList,
+        {
+          carName: name,
+          carYear: year,
+          carMaker: maker,
+          carReview: review,
+          carPrice: price,
+        },
       ]);
-      console.log("runs");
-    }
-    
-  const deleteReview = (car) => {
-    Axios.delete(`http://localhost:3001/api/delete/${car}`); 
-  }
-
-  const updateReview = (movie) => {
-    Axios.put("http://localhost:3001/api/update", {
-      carName: movie,
-      carReview: newReview,
     });
-    setNewReview("")
-  }
+  };
+
+  const getCars = () => {
+    Axios.get("http://localhost:3001/cars").then((response) => {
+      setCarList(response.data);
+    });
+    console.log("getCars :");
+  };
+
+  const updateCarPrice = (id) => {
+    Axios.put("http://localhost:3001/update", { price: newPrice, id: id }).then(
+      (response) => {
+        setCarList(
+          
+          carList.map((val) => {
+            return val.id == id //===
+              ? {
+                  id: val.id,
+                  carName: val.name,
+                  carMaker: val.maker,
+                  carYear: val.year,
+                  carReview: val.review,
+                  carPrice: newPrice,
+                }
+              : val;
+              
+          })
+          
+        );
+      }
+      
+    );
+  console.log("updateCar :");  
+  };
+
+  const deleteCar = (id) => {
+    console.log("deleteCar :");
+    Axios.delete(`http://localhost:3001/delete/${id}`).then((response) => {
+      setCarList(
+        carList.filter((val) => {
+          return val.id !== id; //!= to !==
+        })
+      );
+    });
+  };
 
   return (
+    
     <div className="App">
-      <h1>CRUD APP</h1>
+      <div className="information">
+        <label>Name:</label>
+        <input
+          type="text"
+          onChange={(event) => {
+            setName(event.target.value);
+          }}
+        />
+        <label>Age:</label>
+        <input
+          type="number"
+          onChange={(event) => {
+            setYear(event.target.value);
+          }}
+        />
+        <label>Maker:</label>
+        <input
+          type="text"
+          onChange={(event) => {
+            setMaker(event.target.value);
+          }}
+        />
+        <label>About:</label>
+        <input
+          type="text"
+          onChange={(event) => {
+            setReview(event.target.value);
+          }}
+        />
+        <label>Price:</label>
+        <input
+          type="number"
+          onChange={(event) => {
+            setPrice(event.target.value);
+          }}
+        />
+        <button onClick={addCar}>Add Cars</button>
+      </div>
+      <div className="cars">
+        <button onClick={getCars}>Show cars</button>
 
-      <div className="form">
-      <label>Car Name: </label>
-      <input type="text" name="carName" onChange={(e)=> {
-      setCarName(e.target.value)
-      }}/>
-      <label>Review: </label>
-      <input type="text" name="review" onChange={(e)=> {
-      setReview(e.target.value)}}/>
-      <button onClick={submitReview}>Submit</button>
-      {carReviewList.map((val)=>{
+        {carList.map((val, key) => {
           return (
-            <div className="card">
-              <h1>{val.carName}</h1>
-              <p>{val.carReview}</p>
+            <div className="car">
+              <div>
+                <h3>Name: {val.name}</h3>
+                <h3>Age: {val.year}</h3>
+                <h3>Country: {val.maker}</h3>
+                <h3>Summary: {val.review}</h3>
+                <h3>Price: {val.price}</h3>
+              </div>
+              <div>
+                <input
+                  type="text"
+                  placeholder="increase price... "
+                  onChange={(event) => {
+                    setNewPrice(event.target.value);
+                  }}
+                />
+                <button
+                  onClick={() => {
+                    updateCarPrice(val.id);
+                  }}
+                >
+                  {" "}
+                  Update
+                </button>
 
-              <button onClick={() =>{deleteReview(val.carName)}}>Delete</button>
-              <input type="text" id="updateInput" onChange={(e) =>{
-                setNewReview(e.target.value)
-              }} />
-              <button onClick={()=>{ updateReview(val.carName)}} >Update</button>
+                <button
+                  onClick={() => {
+                    deleteCar(val.id);
+                  }}
+                >
+                  Delete
+                </button>
+              </div>
             </div>
-          )
+          );
         })}
       </div>
     </div>
   );
 }
+
+
+
+//   const submitReview = ()=> {
+//     Axios.post("http://localhost:3001/api/insert", {
+//       carName: carName,
+//       carReview: review,
+//     });
+
+//     setReviewList([...carReviewList, 
+//       {movieName: carName, carReview: review},
+//       ]);
+//       console.log("runs");
+//     }
+    
+//   const deleteReview = (car) => {
+//     Axios.delete(`http://localhost:3001/api/delete/${car}`); //deleted objects have to be set within the parameters of the url
+//   }
+
+//   const updateReview = (car) => {
+//     Axios.put("http://localhost:3001/api/update", {
+//       carName: car,
+//       carReview: newReview,
+//     });
+//     setNewReview("")
+//   }
+
+//   return (
+//     <div className="App">
+//       <h1>CRUD APP</h1>
+
+//       <div className="form">
+//       <label>Car Name: </label>
+//       <input type="text" name="carName" onChange={(e)=> {
+//       setCarName(e.target.value)
+//       }}/>
+//       <label>Review: </label>
+//       <input type="text" name="review" onChange={(e)=> {
+//       setReview(e.target.value)}}/>
+//       <button onClick={submitReview}>Submit</button>
+//       {carReviewList.map((val)=>{
+//           return (
+//             <div className="card">
+//               <h1>{val.carName}</h1>
+//               <p>{val.carReview}</p>
+              
+//               <button onClick={() =>{deleteReview(val.carName)}}>Delete</button>  
+//               <input type="text" id="updateInput" onChange={(e) =>{ //for every delete review we have a different car
+//                 setNewReview(e.target.value)
+//               }} />
+//               <button onClick={()=>{ updateReview(val.carName)}} >Update</button>
+//             </div>
+//           )
+//         })}
+//       </div>
+//     </div>
+//   );
+// }
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -4,6 +4,10 @@ const app = express();
 const mysql = require('mysql');
 const port = 3001;
 
+app.use(cors());
+app.use(express.json()); //allows to grab information from the frontend, Returns middleware 
+app.use(express.urlencoded({extended: true}))
+
 const db = mysql.createPool({
     host: 'localhost',
     user: 'root',
@@ -11,70 +15,121 @@ const db = mysql.createPool({
     database: 'cruddb',
 });
 
-app.use(cors());
-app.use(express.json()); //allows to grab information from the frontend, Returns middleware 
-app.use(express.urlencoded({extended: true}))
 
-//route to get info about existing cars in a json to the frontend from db
-app.get('/api/get', (req, res) => {
-    const sqlSelect = 
-    "Select * FROM car_reviews";   //selecting all info
-    db.query(sqlSelect, [carName, carReview], (err, result) => {
-        console.log("result: " + result);
-        res.send("result: " + result); //sending an array of the results to frontend
-    })
-})
+// // req = require, when we want to get info from frontend
+// // res = response, when we want to send info to frontend
 
-//requesting info from frontend 
-
-app.post("/api/insert/", (req, res) => { //the request from the body and the name of the variable were requesting a variable that is passed by the axios.post from the frontend. 
-
-    //variables to insert
-    const carName = req.body.carName
-    const carReview = req.body.carReview
-
-    //inserting our values to car_reviews db
-    const sqlInsert = 
-    "INSERT INTO car_reviews (carName, carReview) VALUES (?,?)";
-    db.query(sqlInsert, [carName, carReview], (err, result) => {
-    console.log("error" + err);
-
+app.post("/create", (req, res) => {
+    const name = req.body.carName;
+    const year = req.body.carYear;
+    const maker = req.body.carMaker;
+    const review = req.body.carReview;
+    const price = req.body.carPrice;
+  
+    //add values
+    db.query(
+      "INSERT INTO car_reviews (carName, carYear, carMaker, carReview, carPrice) VALUES (?,?,?,?,?)",
+      [name, year, maker, review, price],
+      (err, result) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.send("Values Inserted");
+        }
+      }
+    );
+  });
+  
+  // //route to get info about existing cars in a json to the frontend from db
+  app.get("/car_reviews", (req, res) => {
+    db.query("SELECT * FROM car_reviews", (err, result) => {
+      if (err) {
+        console.log(err);
+        console.log("select car reviews:");
+      } else {
+        res.send(result);
+      }
     });
-});
+  });
+  
+  //update car price
+  app.put("/update", (req, res) => {
+    const id = req.body.id;
+    const price = req.body.price;
+    db.query(
 
-// with "/", it just goes to url. when you say app.get, you pass the route, then the paprameters of a function. 
-// req = require, when we want to get info from frontend
-// res = response, when we want to send info to frontend
+      "UPDATE car_reviews SET carPrice = ? WHERE id = ?",
+      [price, id],
+      (err, result) => {
+        if (err) {
+          console.log(err);
+         // console.log("update backend :");
+        } else {
+          res.send(result);
+        }
+      }
+    );
+  });
+  
+//delete
+  app.delete("/delete/:id", (req, res) => {
+    const id = req.params.id;
+    db.query("DELETE FROM car_reviews WHERE id = ?", id, (err, result) => {
+      if (err) {
+        console.log(err);
+      //  console.log("deleteID :");
+      } else {
+        res.send(result);
+      }
+    });
+  });
+  
 
-app.get("/", (req, res)=> { 
-    console.log("inserting");
-    const sqlInsert = "INSERT INTO `car_reviews` (`carName`, `carReview`) VALUES ('bmw', 'good car');" //testing to see if db is working
-    db.query(sqlInsert, (err, result) => {//creating sql command/insert. we can pass a string or function in the query
-    res.send('hello w0rbv54ld'); //testing sending to server, see if it inserts
-    })
-});
+// app.get('/api/get', (req, res) => {
+//     const sqlSelect = 
+//     "Select * FROM car_reviews";   //selecting all info
+//     db.query(sqlSelect, [carName, carReview], (err, result) => {
+//         console.log("result: " + result);
+//         res.send("result: " + result); //sending an array of the results to frontend
+//     })
+// })
 
+// app.post("/api/insert/", (req, res) => { //the request from the body and the name of the variable were requesting a variable that is passed by the axios.post from the frontend. 
 
-//deleting items from db, method to pass parameters to route
-app.delete('/api/delete/:carName', (req, res)=> {
-    const name = req.params.movieName
-    const sqlDelete = 
-    "DELETE FROM car_reviews WHERE carName = ?"; //deletes spesific car name
+//     //variables to insert
+//     const carName = req.body.carName
+//     const carReview = req.body.carReview
+
+//     //inserting our values to car_reviews db
+//     const sqlInsert = 
+
+//     "INSERT INTO car_reviews (carName, carReview) VALUES (?,?)";
+//     db.query(sqlInsert, [carName, carReview], (err, result) => {
+//     console.log("error" + err);
+
+//     });
+// });
+
+// //deleting items from db, method to pass parameters to route
+// app.delete('/api/delete/:carName', (req, res)=> {
+//     const name = req.params.carName
+//     const sqlDelete = 
+//     "DELETE FROM car_reviews WHERE carName = ?"; //deletes spesific car name
     
-    db.query(sqlDelete, name, (err, result) => {
-        if(err) console.log(err);
-    })
-})
-    app.put('/api/update', (req, res)=> {
-    const name = body.params.carName;
-    const name = body.params.carReview;
-    const sqlUpdate = "UPDATE SET car_reviews carReview = ? WHERE carName= " // ? is the variable were gonna pass
+//     db.query(sqlDelete, name, (err, result) => {
+//         if(err) console.log(err);
+//     })
+// })
+//     app.put('/api/update', (req, res)=> {
+//     const name = body.params.carName;
+//     const name = body.params.carReview;
+//     const sqlUpdate = "UPDATE car_reviews SET carReview = ? WHERE carName= " // ? is the variable were gonna pass
  
     
-    db.query(sqlUpdate, [review, name], (err, result) => { //instead of passing one name, were gonna pass an array
-        if(err) console.log(err);
-    });
-})
+//     db.query(sqlUpdate, [review, name], (err, result) => { //instead of passing one name, were gonna pass an array
+//         if(err) console.log(err);
+//     });
+// })
 
 
 // we want to listen to server, passing port 3001 since localhost is running 3000
