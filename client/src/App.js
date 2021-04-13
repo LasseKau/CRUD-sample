f27f6b47d989c4e43a8dae5879ec28ce35a95699
LasseKau <44494614+LasseKau@users.noreply.github.com>
2021-04-13 18:43:53 +0300

    update
    -

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import './App.css';
 import Axios from 'axios';
+import DragButton from './components/DragItem'
 
 export function App() {
 
--- client/src/components/Int.js
+++
@@ -1,5 +0,0 @@
-// import React, { useState, useEffect } from 'react'
-// import './App.css';
-// import Axios from 'axios';
-// import { useSpring, animated } from "react-spring";
-// import { useDrag } from "react-use-gesture";
